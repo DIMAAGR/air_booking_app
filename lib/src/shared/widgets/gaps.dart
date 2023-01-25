ce159a0dd5e4a4c285f4924cd1b4eed8212e6ccf
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2023-01-25 09:15:14 -0300

    added gaps list

---
+++ lib/src/shared/widgets/gaps.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+const double _gapSize2 = 2;
+const double _gapSize4 = 4;
+const double _gapSize6 = 6;
+const double _gapSize8 = 8;
+const double _gapSize12 = 12;
+const double _gapSize14 = 14;
+const double _gapSize16 = 16;
+const double _gapSize32 = 32;
+const double _gapSize40 = 40;
+const double _gapSize48 = 48;
+const double _gapSize52 = 52;
+const double _gapSize64 = 64;
+const double _gapSize96 = 96;
+const double _gapSize128 = 128;
+
+class HeightGaps {
+  static SizedBox get size2 => const SizedBox(height: _gapSize2);
+  static SizedBox get size4 => const SizedBox(height: _gapSize4);
+  static SizedBox get size6 => const SizedBox(height: _gapSize6);
+  static SizedBox get size8 => const SizedBox(height: _gapSize8);
+  static SizedBox get size12 => const SizedBox(height: _gapSize12);
+  static SizedBox get size14 => const SizedBox(height: _gapSize14);
+  static SizedBox get size16 => const SizedBox(height: _gapSize16);
+  static SizedBox get size32 => const SizedBox(height: _gapSize32);
+  static SizedBox get size40 => const SizedBox(height: _gapSize40);
+  static SizedBox get size48 => const SizedBox(height: _gapSize48);
+  static SizedBox get size52 => const SizedBox(height: _gapSize52);
+  static SizedBox get size64 => const SizedBox(height: _gapSize64);
+  static SizedBox get size96 => const SizedBox(height: _gapSize96);
+  static SizedBox get size128 => const SizedBox(height: _gapSize128);
+  static SizedBox get expand => const SizedBox.expand();
+}
+
+class WidthGaps {
+  static SizedBox get size2 => const SizedBox(width: _gapSize2);
+  static SizedBox get size4 => const SizedBox(width: _gapSize4);
+  static SizedBox get size6 => const SizedBox(width: _gapSize6);
+  static SizedBox get size8 => const SizedBox(width: _gapSize8);
+  static SizedBox get size12 => const SizedBox(width: _gapSize12);
+  static SizedBox get size14 => const SizedBox(width: _gapSize14);
+  static SizedBox get size16 => const SizedBox(width: _gapSize16);
+  static SizedBox get size32 => const SizedBox(width: _gapSize32);
+  static SizedBox get size40 => const SizedBox(width: _gapSize40);
+  static SizedBox get size48 => const SizedBox(width: _gapSize48);
+  static SizedBox get size52 => const SizedBox(width: _gapSize52);
+  static SizedBox get size64 => const SizedBox(width: _gapSize64);
+  static SizedBox get size96 => const SizedBox(width: _gapSize96);
+  static SizedBox get size128 => const SizedBox(width: _gapSize128);
+  static SizedBox get expand => const SizedBox.expand();
+}
